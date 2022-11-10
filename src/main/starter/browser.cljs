adb1fcf004c2dd0a9f2b40017a4ede26f66724ba
Alys Brooks <alysbrooks@fastmail.com>
2022-11-09 22:52:24 -0600

    Add fetch example.

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,4 +1,9 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require [lambdaisland.fetch :as fetch])
+  )
+
+
+(def controller (new js/AbortController))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
@@ -9,6 +14,10 @@
   ;; this is called in the index.html and must be exported
   ;; so it is available even in :advanced release builds
   (js/console.log "init")
+  (js/console.log "fetch")
+  (fetch/get "www.example.com" {:signal (.-signal controller)})
+   
+  (js/console.log (.abort controller))
   (start))
 
 ;; this is called before any code is reloaded
